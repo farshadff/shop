bddd5e9ca6f887a0bf6391f21546f1cf844a5356
farshadff <farshad.badiee@gmail.com>
2019-12-06 01:11:09 +0330

    updatin home theme according to ecco

--- packages/Webkul/Shop/src/Resources/views/home/featured-products.blade.php
+++ packages/Webkul/Shop/src/Resources/views/home/featured-products.blade.php
@@ -1,3 +1,56 @@
+<div class="container-fluid padding-0">
+    <section class="home-hero padding-0">
+        <div class="col-lg-12 padding-0">
+            <img src="{{url('/themes/nicolas/assets/images/ecco1.jpg')}}" alt="">
+            <div class="text-box-1 caption-hero text-center">
+                <h1 class="text-center mt-90 light-color">
+                    با نیکولاس همراه باشید
+                </h1>
+                <h3 class="text-center light-color mt-25">ماجراجویی در قبل طبیعت را با نیکولاس تجربه کنید چون همیشه همراه شما هستیم</h3>
+                <a class="btn btn-1 btn-1e">بیشتر</a>
+
+            </div>
+        </div>
+    </section>
+    <section class="home-hero padding-0">
+        <div class="col-lg-12 padding-0">
+            <img src="{{url('/themes/nicolas/assets/images/ecco2.jpg')}}" alt="">
+            <div class="text-box-2 caption-hero-2 text-center">
+                <h1 class="text-center mt-90 light-color">
+                    با نیکولاس همراه باشید
+                </h1>
+                <h3 class="text-center light-color mt-25">ماجراجویی در قبل طبیعت را با نیکولاس تجربه کنید چون همیشه همراه شما هستیم</h3>
+                <a class="btn btn-1 btn-1e">بیشتر</a>
+            </div>
+        </div>
+    </section>
+    <section class="home-hero padding-0">
+        <div class="col-lg-12 padding-0">
+            <img src="{{url('/themes/nicolas/assets/images/ecco3.jpg')}}" alt="">
+            <div class="text-box-1 caption-hero-3 text-center">
+                <h1 class="text-center mt-90 light-color">
+                    با نیکولاس همراه باشید
+                </h1>
+                <h3 class="text-center light-color mt-25">ماجراجویی در قبل طبیعت را با نیکولاس تجربه کنید چون همیشه همراه شما هستیم</h3>
+                <a class="btn btn-1 btn-1e">بیشتر</a>
+
+            </div>
+        </div>
+    </section>
+    <section class="home-hero padding-0">
+        <div class="col-lg-12 padding-0">
+            <img src="{{url('/themes/nicolas/assets/images/ecco4.jpg')}}" alt="">
+            <div class="text-box-2 caption-hero-2 text-center">
+                <h1 class="text-center mt-90 light-color ">
+                    با نیکولاس همراه باشید
+                </h1>
+                <h3 class="text-center light-color mt-25">ماجراجویی در قبل طبیعت را با نیکولاس تجربه کنید چون همیشه همراه شما هستیم</h3>
+                <a class="btn btn-1 btn-1e">بیشتر</a>
+            </div>
+        </div>
+    </section>
+</div>
+
 @if (app('Webkul\Product\Repositories\ProductRepository')->getFeaturedProducts()->count())
     <div class="container">
         <section class="featured-products">
